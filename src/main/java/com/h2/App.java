bd79dfd6b3be54b070a3d66efb3a307259386faa
herre <herre.r.scherpenzeel@ing.com>
2023-03-07 11:44:00 +0100

    new additions

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,21 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+      public static void main( String[] args )
+      {
+          System.out.println( "Yes Goed !!" );
+       }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
+    }
+
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
